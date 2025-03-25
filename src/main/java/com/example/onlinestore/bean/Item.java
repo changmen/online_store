3b61c3acb811d1b6c52ca5daa10e6d6a88d047aa
changmen.zx <changmen.zx@taobao.com>
2025-03-25 16:09:22 +0800

    商品表增加userId

--- src/main/java/com/example/onlinestore/bean/Item.java
+++ src/main/java/com/example/onlinestore/bean/Item.java
@@ -8,6 +8,7 @@ public class Item implements Serializable {
     @Serial
     private static final long serialVersionUID = 8328093958488219106L;
     private Long id;
+    private Long userId;
     private Long categoryId;
     private String name;
     private String description;
@@ -23,6 +24,14 @@ public class Item implements Serializable {
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
     }
 
     public Long getCategoryId() {
