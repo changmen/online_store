417102dd25fe4962d67f51dd207c47ccbda02e74
changmen.zx <changmen.zx@taobao.com>
2025-04-24 16:56:12 +0800

    优化订单退款逻辑

---
+++ src/main/java/com/example/onlinestore/event/OrderRefundCompletedEventLister.java
@@ -0,0 +1,17 @@
+package com.example.onlinestore.event;
+
+import com.alibaba.fastjson.JSON;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class OrderRefundCompletedEventLister implements ApplicationListener<OrderRefundCompletedEvent> {
+    private static final Logger logger = LoggerFactory.getLogger(OrderRefundCompletedEventLister.class);
+    @Override
+    public void onApplicationEvent(OrderRefundCompletedEvent event) {
+        logger.info("收到退款完成事件，payload:{}", JSON.toJSONString(event.getEventData()));
+
+    }
+}
