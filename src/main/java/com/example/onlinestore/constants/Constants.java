5297ab77b7aba485d6e009de105c0cdda05b4585
changmen <changmen.zx@alibaba-inc.com>
2026-07-01 15:05:58 +0800

    add DEFAULT_PAGE_SIZE constant for pagination support

--- src/main/java/com/example/onlinestore/constants/Constants.java
+++ src/main/java/com/example/onlinestore/constants/Constants.java
@@ -3,6 +3,8 @@ package com.example.onlinestore.constants;
 public class Constants {
     public static final Long ROOT_CATEGORY_PARENT_ID = 0L;
 
+    public static final int DEFAULT_PAGE_SIZE = 20;
+
     //
 
 }
